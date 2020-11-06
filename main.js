1fd3c81f8cba09069b55cd787f531ab31b87869f
ydnaod <65431123+ydnaod@users.noreply.github.com>
2020-11-06 18:18:35 -0500

    add print method to field

--- main.js
+++ main.js
@@ -4,3 +4,33 @@ const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
+
+class Field {
+    constructor(field){
+        this._field = field;
+    }
+
+    print(){
+
+        let stringField = '';
+        for(let i=0; i < this._field.length; i++)
+        {
+            for(let x=0; x < this._field[i].length; x++)
+            {
+                stringField += this._field[i][x];
+            }
+            stringField += '\n';
+        }
+        console.log(stringField);
+    }
+  }
+
+  const myField = new Field([
+    ['*', '░', 'O'],
+    ['░', 'O', '░'],
+    ['░', '^', '░'],
+  ]);
+
+ myField.print();
+
+  
